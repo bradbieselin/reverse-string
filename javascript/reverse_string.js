6a5925d87ae0d3cac809a96ecb04ca5b3b3abaa9
Brad Bieselin <bradley.bieselin@gmail.com>
2022-05-19 22:26:49 -0400

    commit tree ec82db61cbe25acc281432a3518cbdf0eae52583

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,9 +1,13 @@
 function reverseString(str) {
-  // type your code here
+  let arr = [];
+  for(let i = 0; i < str.length; i++) {
+    arr.unshift(str[i]);
+  }
+  arr.join("");
+  return arr;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
@@ -17,3 +21,5 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//Reverse any given string, such that the first letter becomes the last, the 2nd letter becomes second to last, last letter becomes first letter, etc.
